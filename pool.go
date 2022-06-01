01f0ac05e838d86790932bc335663ee776a70f66
马以 <gxstaxant@gmail.com>
2022-06-01 19:14:49 +0800

    commit workerpool

--- pool.go
+++ pool.go
@@ -1,16 +1,107 @@
 package workerpool
 
 import (
-	"runtime/trace"
+	"errors"
+	"fmt"
 	"sync"
 )
+
+var defaultCapacity = 100	// 默认池大小
+var maxCapacity = 2000 		// 最大池大小
+
+type Task func()
 
 type Pool struct {
 	capacity int    		// workerpool 大小
 
 	active chan struct{}	// active channel
-	task chan trace.Task
+	tasks chan Task
 
 	wg sync.WaitGroup		// 用于在pool销毁时等待所有worker退出
 	quit chan struct{}		// 用于通知各个worker退出的信号channel
 }
+
+func New(capacity int) *Pool {
+	if capacity < 0 {
+		capacity = defaultCapacity
+	}
+	if capacity > maxCapacity {
+		capacity = maxCapacity
+	}
+
+	p := &Pool{
+		capacity: capacity,
+
+		active: make(chan struct{}, capacity),
+		tasks: make(chan Task),
+		quit: make(chan struct{}),
+	}
+
+	go p.run()
+
+	return p
+}
+
+func (p *Pool) run() {
+	idx := 0
+	for {
+		select {
+		case <-p.quit:
+			return
+		case p.active <- struct{}{}:
+			// 创建 worker
+			idx++
+			p.newWorker(idx)
+		}
+	}
+}
+
+func (p *Pool) newWorker(i int) {
+	p.wg.Add(1)
+	go func() {
+		// 消除 panic，不让其向上传到到调用函数
+		defer func() {
+			if err := recover(); err != nil {
+				fmt.Printf("worker[%03d]: revover panic[%s] and exit\n", i, err)
+				<-p.active
+			}
+			p.wg.Done()
+		}()
+
+		fmt.Printf("worker[%03d]: start\n", i)
+
+		for {
+			select {
+			case <-p.quit:
+				fmt.Printf("worker[%03d]: exit\n", i)
+				<-p.active
+				return
+			case t := <-p.tasks:
+				fmt.Printf("worker[%03d]: receive a task\n", i)
+				t()
+			}
+		}
+	}()
+
+}
+
+var ErrWorkerPoolFreed = errors.New("worker pool freed") // worker pool 释放
+
+func (p *Pool) Schedule(t Task) error {
+	select {
+	case <-p.quit:
+		return ErrWorkerPoolFreed
+	case p.tasks <- t:
+		return nil
+	}
+}
+
+func (p * Pool) Free() {
+
+	// p.quit <- struct{}{} 我开始使用的是这个，
+	// 结果会死锁，我想大概是和java中的signal和signalAll的区别吧
+	//close(p.quit)
+
+	p.wg.Wait()
+}
+
